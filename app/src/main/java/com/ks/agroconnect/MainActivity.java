53de114e5041ebd474a060d05a7206c2cfd7d7bd
Kishan Kr Sharma <skishan781@gmail.com>
2020-05-12 18:26:50 +0530

    HomeScreen Added & Dashboard

--- app/src/main/java/com/ks/agroconnect/MainActivity.java
+++ app/src/main/java/com/ks/agroconnect/MainActivity.java
@@ -2,13 +2,21 @@ package com.ks.agroconnect;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.ActionBar;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -29,19 +37,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        if (true){
-            startActivity(new Intent(MainActivity.this,HomeActivity.class));
-        }
         email = findViewById(R.id.login_email);
         pass = findViewById(R.id.login_password);
         btnLogin = findViewById(R.id.login_btn);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         //Progress Dialog Created
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setCancelable(false); // if you want user to wait for some process to finish,
         builder.setView(R.layout.loading_dialog);
         final AlertDialog dialog = builder.create();
+
+
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,20 +67,24 @@ public class MainActivity extends AppCompatActivity {
                     pass.setError("Required Field...");
                     return;
                 }
-
-                dialog.show();//Showing the Progress dialog
-
                 mSharedPreferences = getSharedPreferences("accountRa", MODE_PRIVATE);
                 editor = mSharedPreferences.edit();
 
                 String e = mSharedPreferences.getString("email", null);
                 String p = mSharedPreferences.getString("password", null);
                 if(mEmail.equals(e) && mPass.equals(p)){
-                    startActivity(new Intent(MainActivity.this, HomeActivity.class));
+
+                    dialog.show();
+                    new Handler().postDelayed(new Runnable() {
+                        public void run() {
+                            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                            dialog.cancel();
+                        }
+                    }, 1000);
+                    //startActivity(new Intent(MainActivity.this, HomeActivity.class));
                 }else {
                     Toast.makeText(MainActivity.this, "Error! TRY AGAIN", Toast.LENGTH_LONG).show();
                 }
-
             }
         });
 
@@ -80,9 +93,32 @@ public class MainActivity extends AppCompatActivity {
         signUp_text.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+                startActivity(new Intent(MainActivity.this, RegisterActivity.class));
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logout_menu:
+                Toast.makeText(MainActivity.this, item.getTitle().toString(),Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.about_menu:
+                Toast.makeText(MainActivity.this, item.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.setting_menu:
+                Toast.makeText(MainActivity.this, item.getTitle().toString(),Toast.LENGTH_SHORT).show();
+                return true;
+            default: super.onOptionsItemSelected(item);
+        }
+        return false;
     }
 }
